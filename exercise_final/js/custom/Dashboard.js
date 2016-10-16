b69dbd7ee13f9a59421c812143d8b9007c03fa7e
mmoreno-cart <mmoreno.topografia@gmail.com>
2016-10-16 20:07:40 +0200

    add code

---
+++ exercise_final/js/custom/Dashboard.js
@@ -0,0 +1,182 @@
+/* DOM */
+var dom = {
+	chart1: $('#chart-1'),
+	chart2: $('#chart-2'),
+	chart3: $('#chart-3'),
+	chart4: $('#chart-4'),
+	chart5: $('#chart-5'),
+	table: $('#table')
+};
+
+/* Crossfilter */
+var cf;
+var filterFromCode = false;
+
+/* Asignación gráficos */
+var charts = {};
+
+// Load dashboard from received data
+function initDashboard(data){
+	
+	//console.log(data);
+	/*data.data.forEach(function (d) {
+		console.log(d.data_id,d.latitude,d.longitude);
+	});*/
+	
+	if (!map){
+		initMap('map');		
+	}
+	
+	/* Ajuste de datos */
+	var clean_data = [];
+	var admin1_list = [];
+	data.forEach(function (d) {
+		if (admin1_list.indexOf(d.admin1) == -1){admin1_list.push(d.admin1);}
+		if (d.latitude && d.longitude) { //check that we have coordinates
+			if (!d.event_type || d.event_type == '') {
+				d.event_type = 'Not available';
+			}
+			if (!d.admin1 || d.admin1 == '') {
+				d.admin1 = 'Not available';
+			}
+			if (!d.source || d.source == '') {
+				d.source = 'Not available';
+			}
+			var dateFormat = d3.time.format('%Y-%m-%d'); // Format in which data come
+			var event_date_split = d.event_date.split('-');
+			d.event_date = (new Number(event_date_split[0]) + 2000) + '-' + event_date_split[1] + '-' + event_date_split[2];
+			d.dd = dateFormat.parse(d.event_date);
+			d.month = d3.time.month(d.dd);
+			d.close = +d.close; // coerce to number
+			d.open = +d.open;
+			clean_data.push(d);
+		}
+	});
+	console.log(admin1_list);
+	/* Crossfilter config */
+	cf = crossfilter(clean_data);
+	
+	// Dimensions
+	cf.dim = {};
+	cf.dim.event_type = cf.dimension(function(d){ return d.event_type; });
+	cf.dim.admin1 = cf.dimension(function(d){ return d.admin1; });
+	cf.dim.data_id = cf.dimension(function(d){ return d.data_id; });
+	cf.dim.latitude = cf.dimension(function(d){ return +d.latitude; });
+	cf.dim.longitude = cf.dimension(function(d){ return +d.longitude; });
+	cf.dim.dd = cf.dimension(function (d) { return d.dd; });
+	
+	// Groups
+	cf.grp = {};
+	cf.grp.event_type = cf.dim.event_type.group();
+	cf.grp.admin1 = cf.dim.admin1.group().reduceSum(function(d) { return d.fatalities; });
+	cf.grp.data_id = cf.dim.data_id.group();
+	cf.grp.latitude = cf.dim.latitude.group();
+	cf.grp.longitude = cf.dim.longitude.group();
+	cf.grp.dd_COUNT = cf.dim.dd.group().reduceSum(function(d) { return d.fatalities; });//.reduceCount();
+
+	cf.grp.all = cf.groupAll();
+	
+	/* Charts */
+	charts.admin1 = dc.rowChart('#chart-1');
+	charts.event_type = dc.pieChart('#chart-2');
+	//charts.source = dc.rowChart('#chart-3');
+	charts.event_date = dc.barChart('#chart-5');
+	charts.Table = dc.dataTable('.dc-data-table');
+
+	$(dom.chart1).html('<h4>1st admin level (Top 10)</h4>');
+	charts.admin1
+		.width(250)
+		.height(calculateChartHeight(cf.grp.admin1.size(),10, 25, 40))//
+		.dimension(cf.dim.admin1)
+		.group(cf.grp.admin1)
+		.elasticX(true)
+		.data(function(group) {	return group.top(10); })
+		.colors(['#6DDC7A'])
+		.colorAccessor(function(d, i){ return 0;})
+		.xAxis().ticks(3);//.tickFormat(d3.format("g"));
+	
+	$(dom.chart2).html('<h4>Event type</h4>');
+	charts.event_type
+        .dimension(cf.dim.event_type)
+        .group(cf.grp.event_type)
+        .width(180)
+        .height(180)
+        .radius(80)
+        .innerRadius(30);
+	
+	// Date chart
+    charts.event_date.width(1000)
+        .height(120)
+        .margins({top: 0, right: 50, bottom: 20, left: 40})
+        .dimension(cf.dim.dd)
+        .group(cf.grp.dd_COUNT)
+        .centerBar(true)
+        .gap(1)
+        .x(d3.time.scale().domain([new Date(2016, 0, 1), new Date(2017, 0, 1)]))
+		.yAxis().ticks(3);
+		
+	dc.dataCount("#filter-indicator")
+		.dimension(cf)
+		.group(cf.grp.all);
+	
+	charts.Table
+        .dimension(cf.dim.event_type)
+        // Data table does not use crossfilter group but rather a closure
+        // as a grouping function
+        .group(function (d) {
+			return d.event_type;
+        })
+        // (_optional_) max number of records to be shown, `default = 25`
+        .size(cf.grp.data_id.size())
+        // There are several ways to specify the columns; see the data-table documentation.
+        // This code demonstrates generating the column header automatically based on the columns.
+        .columns([			
+			function(d) {
+				return d.location;
+			},
+			function(d) {
+				return d.admin1;
+			},
+			function(d) {
+				return d.fatalities;
+			}
+		])
+        // (_optional_) sort using the given field, `default = function(d){return d;}`
+        .sortBy(function (d) {
+            return d.location;
+        })
+        // (_optional_) sort order, `default = d3.ascending`
+        .order(d3.ascending)
+        // (_optional_) custom renderlet to post-process chart using [D3](http://d3js.org)
+        .on('renderlet', function (table) {
+            table.selectAll('.dc-table-group').classed('info', true);
+        });
+	
+	for (var i = 0; i < dc.chartRegistry.list().length; i++) {
+		var chartI = dc.chartRegistry.list()[i];
+		chartI.on("filtered", function(chart, filter){ if (filterFromCode === false) { chartFiltered(); }});
+	}
+	
+	map.on("moveend", onmapmoveend);
+	
+	updateMapPoints();
+	
+	setTimeout(function(){
+		map.fitBounds(cluster_layer.getBounds());
+	},1000);
+	
+	
+	//geofilter();
+	dc.renderAll();
+	//filterFromCode = false;
+	
+}
+
+// Function to calculate the height  of row charts
+function calculateChartHeight(value_number, max_rows, row_height, gaps) {
+	var n;
+	var h;
+	(value_number < max_rows) ? n=value_number : n=max_rows;
+	h = n * row_height + gaps;
+	return h;
+}
---
+++ exercise_final/js/custom/Map.js
@@ -0,0 +1,70 @@
+/* Map */
+var map;
+var cluster_layer;
+var info;
+
+// load map
+function initMap(idmap){
+	
+	// Basemaps
+	var OpenStreetMap_HOT = L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
+	});
+	var CartoDB_Positron = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+		subdomains: 'abcd',
+		maxZoom: 19
+	});
+	/*var Geoeuskadi = L.tileLayer.wms("http://www.geo.euskadi.eus/WMS_ORTOARGAZKIAK", {
+		layers: 'ORTO_EGUNERATUENA_MAXIMA_ACTUALIZADA',
+		format: 'image/png',
+		//transparent: true,
+		attribution: "Eusko Jaurlaritza / Gobierno Vasco"
+	});*/
+	var basemaps = {
+		'HOT': OpenStreetMap_HOT,
+		'Claro': CartoDB_Positron,
+		//'Satélite': Geoeuskadi
+	};
+	
+	// Map definition
+	map = L.map(idmap,{
+		center: [20, 0], // y,x
+		zoom: 1,
+		maxZoom:16,
+		minZoom:1,
+		layers: [OpenStreetMap_HOT],
+		attributionControl: true
+	});
+	
+	// Cluster layer for events (empty at the start)
+	cluster_layer = L.markerClusterGroup({
+		showCoverageOnHover		: false,
+		maxClusterRadius		: 45,
+		//disableClusteringAtZoom	: 8,
+		spiderfyOnMaxZoom		: true
+	});
+	var overlays = {
+		"ACLED events": cluster_layer,
+	};
+	map.addLayer(cluster_layer);
+	
+	L.control.layers(basemaps, overlays).addTo(map);
+	
+	// control that shows the location of the event on hover
+	info = L.control({ position: 'bottomleft'});
+	info.onAdd = function (map) {
+		if ($('.info').length < 1) {
+			this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+		}		
+		this.update();
+		return this._div;
+	};
+	info.update = function (title) {
+		this._div.innerHTML = (title ?
+			'<b>' + title + '</b>'
+			: 'Hover over an event');
+	};
+	
+}
